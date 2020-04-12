91fe5f4758f6a7364baf114d18e92c0925a3084c
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2020-04-12 23:34:45 +0700

    Add files via upload

---
+++ data/Titik_Foodtrack_2.js
@@ -0,0 +1,16 @@
+var json_Titik_Foodtrack_2 = {
+"type": "FeatureCollection",
+"name": "Titik_Foodtrack_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "_ID": "1", "_LATITUDE": -7.7660077404000001, "_LONGITUDE": 110.379723497, "_ELEVATION": 0.0, "_nama": "Gudeg Yu Djum" }, "geometry": { "type": "Point", "coordinates": [ 110.379723496735096, -7.766007740401668 ] } },
+{ "type": "Feature", "properties": { "_ID": "2", "_LATITUDE": -7.8002283664999998, "_LONGITUDE": 110.375117809, "_ELEVATION": 0.0, "_nama": "Rujak Es Krim Pak Nardi" }, "geometry": { "type": "Point", "coordinates": [ 110.375117808580399, -7.800228366472514 ] } },
+{ "type": "Feature", "properties": { "_ID": "3", "_LATITUDE": -7.7999855473000004, "_LONGITUDE": 110.376197733, "_ELEVATION": 0.0, "_nama": "Museum Puro Pakualaman" }, "geometry": { "type": "Point", "coordinates": [ 110.37619773298502, -7.799985547268491 ] } },
+{ "type": "Feature", "properties": { "_ID": "4", "_LATITUDE": -7.7948946158999997, "_LONGITUDE": 110.365756229, "_ELEVATION": 0.0, "_nama": "Lumpia Samijaya" }, "geometry": { "type": "Point", "coordinates": [ 110.36575622856617, -7.794894615881755 ] } },
+{ "type": "Feature", "properties": { "_ID": "5", "_LATITUDE": -7.8051577983999998, "_LONGITUDE": 110.36526136099999, "_ELEVATION": 0.0, "_nama": "Special Bakmi dan Nasi Goreng Pak Pele" }, "geometry": { "type": "Point", "coordinates": [ 110.365261361002922, -7.805157798350454 ] } },
+{ "type": "Feature", "properties": { "_ID": "6", "_LATITUDE": -7.813, "_LONGITUDE": 110.363, "_ELEVATION": 0.0, "_nama": "Warung Brongkos Handayani" }, "geometry": { "type": "Point", "coordinates": [ 110.362998751016249, -7.812700930032137 ] } },
+{ "type": "Feature", "properties": { "_ID": "8", "_LATITUDE": -7.812, "_LONGITUDE": 110.363, "_ELEVATION": 0.0, "_nama": "Alun-Alun Kidul" }, "geometry": { "type": "Point", "coordinates": [ 110.363251407696978, -7.811830802947053 ] } },
+{ "type": "Feature", "properties": { "_ID": "7", "_LATITUDE": -7.805, "_LONGITUDE": 110.364, "_ELEVATION": 0.0, "_nama": "Keraton Yogyakarta" }, "geometry": { "type": "Point", "coordinates": [ 110.364250186454186, -7.805289962074649 ] } },
+{ "type": "Feature", "properties": { "_ID": "0", "_LATITUDE": 0.0, "_LONGITUDE": 0.0, "_ELEVATION": 0.0, "_nama": "Angkringan Kopi Jos" }, "geometry": { "type": "Point", "coordinates": [ 110.36565805404507, -7.788883697412377 ] } }
+]
+}
